9b1fed75a9121d1f3d3a8800f0c27ffa86985879
buxii <140445126+buxii@users.noreply.github.com>
2023-07-29 04:41:09 +0200

    Puzzle01.cpp

--- 01/Puzzle01.cpp
+++ 01/Puzzle01.cpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <algorithm>
 
-using namespace std;
-
 /*
 1000
 2000	//6000
@@ -25,11 +23,10 @@ using namespace std;
 R/ 24000!!
 */
 
-
-vector <string> crear_vector_strings() {
-	vector <string> vect;
-	ifstream input_file("puzzle01.txt");
-	string calories_linea;
+std::vector <std::string> crear_vector_strings() {
+	std::vector <std::string> vect;
+	std::ifstream input_file("puzzle01.txt");
+	std::string calories_linea;
 	if (input_file.is_open()) {
 		while (!input_file.eof()) {
 			getline(input_file, calories_linea);
@@ -41,7 +38,7 @@ vector <string> crear_vector_strings() {
 
 auto passar_string_a_vector_de_vectors_de_int(std::vector<std::string> const& s) {
 	std::vector <std::vector<int>> resultat;
-	resultat.push_back({}); //creem un element al vector per assegurar-nos que començi bé
+	resultat.push_back({}); //creem un element al vector per assegurar-nos que comenï¿½i bï¿½
 	for (auto const& it : s) {
 		if (it.size() > 0) {
 			resultat.back().push_back(std::stoi(it));
@@ -53,8 +50,8 @@ auto passar_string_a_vector_de_vectors_de_int(std::vector<std::string> const& s)
 	return resultat;
 }
 
-vector <int> suma_vector_de_vectors_int(const vector <vector<int>>& a_sumar) { 
-	vector <int> sumat;
+std::vector <int> suma_vector_de_vectors_int(const std::vector <std::vector<int>>& a_sumar) {
+	std::vector <int> sumat;
 	for (const auto& vec : a_sumar) {
 		int suma = 0;
 		for (int num : vec) {
@@ -66,14 +63,14 @@ vector <int> suma_vector_de_vectors_int(const vector <vector<int>>& a_sumar) {
 }
 
 int main() {
-	vector<string> vector_strings = crear_vector_strings(); //posem el fitxer dins del vector de strings
+	std::vector<std::string> vector_strings = crear_vector_strings(); //posem el fitxer dins del vector de strings
 
 	auto vector_int = passar_string_a_vector_de_vectors_de_int(vector_strings); //passem el vector de strings a un vector de vector d'ints
 
-	vector<int> vector_sumat = suma_vector_de_vectors_int(vector_int);
+	std::vector<int> vector_sumat = suma_vector_de_vectors_int(vector_int);
 
 	int maxim_calories_1 = 0;
-	
+
 	//Problema 1:
 
 	for (auto it : vector_sumat) {
@@ -82,18 +79,18 @@ int main() {
 		}
 	}
 
-	cout << "RESULTAT 1: " << maxim_calories_1 << endl;
+	std::cout << "RESULTAT 1: " << maxim_calories_1 << std::endl;
 
 	//Problema 2:
 
-	//ordenem el vector i el posem en reversa per poder agafar els 3 primers i que siguin els mes grans (mes fàcil que fer .size(), .size()-1, .size()-2.
-	sort(vector_sumat.begin(),vector_sumat.end()); 
-	reverse(vector_sumat.begin(),vector_sumat.end());
+	//ordenem el vector i el posem en reversa per poder agafar els 3 primers i que siguin els mes grans (mes fï¿½cil que fer .size(), .size()-1, .size()-2.
+	sort(vector_sumat.begin(), vector_sumat.end());
+	reverse(vector_sumat.begin(), vector_sumat.end());
 
 	int resultat_2 = vector_sumat[0] + vector_sumat[1] + vector_sumat[2];
 
 
-	cout << "RESULTAT 2: " << resultat_2 << endl;
+	std::cout << "RESULTAT 2: " << resultat_2 << std::endl;
 
 	return 0;
 }
